c6f4be06ecb4ceccc93f64686aa9106c2aa51331
ming <ming@pluvio.io>
2018-08-31 19:46:53 +0800

    dotenv test

--- app/application.py
+++ app/application.py
@@ -1,10 +1,12 @@
+import os
+import settings
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World, Update wsgi"
+    return "Hello World, Update wsgi" + ":" + os.getenv('ABC')
 
 if __name__ == '__main__':
     app.run(debug=True)
